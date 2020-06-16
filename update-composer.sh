d22d8ad8c698559534e3ec8ee1ded06fee768a63
alexonbstudio <alexonbstudio@gmail.com>
2020-06-16 23:05:51 +0200

    Update update-composer.sh

--- update-composer.sh
+++ update-composer.sh
@@ -9,9 +9,11 @@ if [ ! -x composer ]; then
 
 	# Update composer
 	composer self-update
-
+	composer update --no-dev
+	composer dump-autoload --no-dev
+	composer clearcache	
 else
-	curl -O  | bash
+	curl -O https://raw.githubusercontent.com/website-project-WP/composer-wp/master/install-composer.sh | bash
 
 fi
 
